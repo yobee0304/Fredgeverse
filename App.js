5f7e5344e2200ae34eab4a04dfd0ba5164c4af6f
yobee0304 <chan20172@gmail.com>
2024-09-12 11:36:34 +0900

    Add DetailIngredient screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screen/home';
 import AddIngredients from './screen/ingredient/addIngredients';
 import SearchIngredients from './screen/ingredient/searchIngredients';
+import DetailIngredient from './screen/ingredient/detailIngredient';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Home" component={Home} options={{ title: '홈' }}/>
         <Stack.Screen name="AddIngredients" component={AddIngredients} options={{ title: '재료 추가하기' }}/>
         <Stack.Screen name="SearchIngredients" component={SearchIngredients} options={{ title: '재료 조회하기' }}/>
+        <Stack.Screen name="DetailIngredient" component={DetailIngredient} options={{ title: '재료 상세정보' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
