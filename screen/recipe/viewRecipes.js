daf037342dc634de5d85df4f54a22aee9aa6525f
yobee0304 <chan20172@gmail.com>
2024-09-19 11:39:43 +0900

    Init ViewRecipes screen

---
+++ screen/recipe/viewRecipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const ViewRecipes = () => {
+    return (
+        <Text>레시피 조회 화면입니다.</Text>
+    );
+};
+
+export default ViewRecipes;
